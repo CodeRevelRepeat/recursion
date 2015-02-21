056d5fb865c2c82d79f073901a3b2bdf880cbd66
Lauren <lhspiegel@gmail.com>
2015-02-20 17:24:20 -0800

    Implements getElementsByClassName but elements are appearing in reversed order

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,40 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+
+	var foundElements = [];
+	
+
+	var checkNode = function(node, className){
+
+		if(node.nodeType === document.ELEMENT_NODE && node.childNodes.length > 0){
+			for(var i=0; i< node.childNodes.length; i++){
+				//if(node.childNodes[i].className.toUpperCase() === className.toUpperCase()){
+					//foundElements.push(node.childNodes[i]);
+
+				checkNode(node.childNodes[i], className);
+				}
+			}
+
+		if(node.nodeType === document.ELEMENT_NODE){
+			if(node.className.toUpperCase() === className.toUpperCase()){
+				console.log("node going in: " + node);
+				foundElements.push(node);
+			}
+		}
+	};
+
+	checkNode(document.body, className);
+
+	console.log(foundElements);
+	//console.log(foundElements.reverse());
+
+	//var reversedElements = foundElements.reverse();
+	//var reversedElements = [];
+
+	// for(var j= foundElements.length-1; j>=0; j--){
+	// 	reversedElements.push(foundElements[j]);
+	// }
+	return foundElements;
+ 
 };
