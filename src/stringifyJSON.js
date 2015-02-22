5d37c5a8fcab850f988d20eab7566e493c91d1b0
Lauren <lhspiegel@gmail.com>
2015-02-22 14:58:54 -0800

    First attempt implementing JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,98 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	var checkStringNumberBoolean = function(item){
+		if(typeof item === 'number' || typeof item === 'string' || typeof item === 'boolean'){
+			return true;
+		}
+
+	}
+
+	var stringifiedJSONArray = [];	
+
+	var subArrayCounter = 0;
+
+	var stringifyParts = function(obj, arrayLevel){
+
+
+
+		if(Array.isArray(obj)){
+				
+				//subArray.push(furtherArray);
+				for(var i=0; i< obj.length; i++){
+					
+					stringifyParts(obj[i], arrayLevel[subArrayCounter]);
+					subArrayCounter++;
+					
+				}
+		}
+
+	
+
+
+		 if(typeof obj === 'number'){
+		    arrayLevel.push(obj);
+		   }
+
+		  if(typeof obj === 'string'){
+		    arrayLevel.push(obj);
+		   }
+
+		  if(typeof obj === 'boolean'){
+		    	arrayLevel.push(obj);
+		    }
+
+
+		  if(obj && obj.hasOwnProperty('{}')){		
+				var newObj = {};
+					for(var key in obj){
+
+						if(typeof key === 'number' || typeof key === 'string' && checkStringNumberBoolean(obj[key])){
+							newObj[key] = obj[key];
+
+							}
+						}
+
+					arrayLevel.push(newObj);
+				}
+
+		  else{
+
+		  	// if(arrayLevel != stringifiedJSONArray){
+		  	// 	arrayLevel.push('null');
+		  	// }
+		    	//return {};
+		    	// or null if this other element was within an array?  Can use if arrayLevel === stringifiedJSONArray to determine whether in array or not.
+		    }
+
+	};
+
+
+
+
+			//need to loop through arrays and objects.
+
+
+			//recursive case to flatten array?
+		 //    else{
+		  
+		 //  	var flattenedArray = obj.reduce(function(a, b){
+		 //  		return a.concat(b);
+		 //  	})
+
+		 //  	stringifyParts(flattenedArray);
+
+		 //  	}
+
+  	// 	}
+
+  	// };
+
+  	stringifyParts(obj, stringifiedJSONArray);
+
+
+  	var stringifiedJSON = stringifiedJSONArray.join("");
+
+  	return stringifiedJSON;
 };
