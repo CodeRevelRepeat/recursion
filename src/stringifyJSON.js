b9b43ca61c822882bb82373d893e628e22493548
Lauren <lhspiegel@gmail.com>
2015-02-23 14:34:06 -0800

    Fixes stringify for objects that are neither arrays nor primitives.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,17 +5,14 @@
 
 var stringifyJSON = function(obj) {
 
-
-var masterResult;
-
-
+//Function to deal with converting an array to JSON string:
 
 	var stringifyArray = function(array){
 
 		var result = [];
 
 		for(var i=0; i< array.length; i++){
-			console.log("doing the array thing");
+	
 			var value = array[i];
 
 			if(typeof value === 'undefined' || typeof value === 'function' || typeof value === 'symbol'){
@@ -28,158 +25,69 @@ var masterResult;
 				}
 
 			}
-			console.log("done array looping");
-
-			console.log(result);
+		
 			return result; 
 	};
 
 
+//Function to deal with converting the keys and properties of inner object to JSON string:
+
 	var stringifyObject = function(innerObj){
 
-		var newObj = {};
+		var stringObj = '{';
 
 
 		for(var key in innerObj){
 				if(typeof key === 'number' || typeof key === 'string' && 
-					typeof innerObj[key] === 'number' || typeof innerObj[key] === 'string' || typeof innerObj[key] === 'boolean'){
+					typeof innerObj[key] === 'number' || typeof innerObj[key] === 'string' || typeof innerObj[key] === 'boolean'
+					|| innerObj[key] === null || typeof innerObj[key] === 'object'){
 
 
-				newObj[stringifyJSON(key)] = stringifyJSON(innerObj[key]);
+				stringObj = stringObj + stringifyJSON(key) + ":" + stringifyJSON(innerObj[key]) + ",";
 				}
 			}
 
-		return newObj;
+			//To trim comma off of last property:
+			if(stringObj.length >= 2){
+					stringObj = stringObj.substr(0, stringObj.length - 1);
+				}
+
+			stringObj = stringObj + '}'
+
+		return stringObj;
 
 	};
 				
 
-
-				if(obj === null){
-					return "null";
-				}
-
-
-				if(typeof obj === 'number'){
-					console.log('thinks i am a primitive number')
-					return String(obj);
-				}
-
-				if(typeof obj === 'string'){
-					return ('"' + obj + '"');
-
-				}
-
-				if(typeof obj === 'boolean'){
-					return obj.toString();
-				}
+	if(obj === null){
+		return "null";
+		}
 
 
-				if(Array.isArray(obj)){
-					console.log("thinks i'm an array")
-					masterResult = '[' + stringifyArray(obj).toString() + ']';
-				}
+	if(typeof obj === 'number'){
+			return String(obj);
+		}
 
-				if(typeof obj === 'object' && !Array.isArray(obj)){
-					console.log("thinks i'm a default object");
-					masterResult = stringifyObject(obj);
-				}
+	if(typeof obj === 'string'){
+			return ('"' + obj + '"');
+		}
 
-		
-		//If don't need masterResult as an array, just get rid of it and return values.
+	if(typeof obj === 'boolean'){
+			return obj.toString();
+		}
 
-		//if(masterResult.length === 1){
+	if(Array.isArray(obj)){
+			return '[' + stringifyArray(obj).toString() + ']';
+		}
 
-		//	return masterResult[0];
-		//}
+	if(typeof obj === 'object' && !Array.isArray(obj)){
+			return stringifyObject(obj);
+		}
 
-		//else{
-		//	return masterResult;
-		//}
 
-			return masterResult;
-
-		};
+	};
 
 
 
 
-		// if(Array.isArray(obj)){
-				
-		// 		//subArray.push(furtherArray);
-		// 		for(var i=0; i< obj.length; i++){
-					
-		// 			stringifyParts(obj[i], arrayLevel[subArrayCounter]);
-		// 			subArrayCounter++;
-					
-		// 		}
-		// }
 
-	
-
-
-		//  if(typeof obj === 'number'){
-		//     arrayLevel.push(obj);
-		//    }
-
-		//   if(typeof obj === 'string'){
-		//     arrayLevel.push(obj);
-		//    }
-
-		//   if(typeof obj === 'boolean'){
-		//     	arrayLevel.push(obj);
-		//     }
-
-
-		//   if(obj && obj.hasOwnProperty('{}')){		
-		// 		var newObj = {};
-		// 			for(var key in obj){
-
-		// 				if(typeof key === 'number' || typeof key === 'string' && checkStringNumberBoolean(obj[key])){
-		// 					newObj[key] = obj[key];
-
-		// 					}
-		// 				}
-
-		// 			arrayLevel.push(newObj);
-		// 		}
-
-		//   else{
-
-		//   	// if(arrayLevel != stringifiedJSONArray){
-		//   	// 	arrayLevel.push('null');
-		//   	// }
-		//     	//return {};
-		//     	// or null if this other element was within an array?  Can use if arrayLevel === stringifiedJSONArray to determine whether in array or not.
-		//     }
-
-	// };
-
-
-
-
-			//need to loop through arrays and objects.
-
-
-			//recursive case to flatten array?
-		 //    else{
-		  
-		 //  	var flattenedArray = obj.reduce(function(a, b){
-		 //  		return a.concat(b);
-		 //  	})
-
-		 //  	stringifyParts(flattenedArray);
-
-		 //  	}
-
-  	// 	}
-
-//   	// };
-
-//   	stringifyParts(obj);
-
-
-//   	var stringifiedJSON = stringifiedJSONArray.join("");
-
-//   	return stringifiedJSON;
-// };
