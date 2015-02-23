d3164d7486b99c9410cd47e63408c060d948fe10
Lauren <lhspiegel@gmail.com>
2015-02-23 12:26:51 -0800

    Modifies new approach to somewhat functional but still pushing into master array and not passing tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,71 +5,144 @@
 
 var stringifyJSON = function(obj) {
 
-	var checkStringNumberBoolean = function(item){
-		if(typeof item === 'number' || typeof item === 'string' || typeof item === 'boolean'){
-			return true;
-		}
 
-	}
-
-	var stringifiedJSONArray = [];	
-
-	var subArrayCounter = 0;
-
-	var stringifyParts = function(obj, arrayLevel){
+var masterResult = [];
 
 
 
-		if(Array.isArray(obj)){
-				
-				//subArray.push(furtherArray);
-				for(var i=0; i< obj.length; i++){
-					
-					stringifyParts(obj[i], arrayLevel[subArrayCounter]);
-					subArrayCounter++;
-					
+	var stringifyArray = function(obj){
+
+		var result = [];
+
+
+		for(var i=0; i< obj.length; i++){
+			console.log("doing the array thing");
+			var value = obj[i];
+
+			if(typeof value === 'undefined' || typeof value === 'function' || typeof value === 'symbol'){
+				result.push('null');
+
 				}
+
+			else {
+				result.push(stringifyJSON(value));
+				}
+
+			masterResult.push(result);
+			}
+	};
+
+
+	var stringifyObject = function(obj){
+
+		var newObj = {};
+
+
+		for(var key in obj){
+				newObj[stringifyJSON(key)] = stringifyJSON(obj[key]);
 		}
+
+	};
+				
+
+
+				if(obj === null){
+					masterResult.push("null");
+				}
+
+
+				if(typeof obj === 'number'){
+					console.log('thinks i am a primitive number')
+					masterResult.push(String(obj));
+				}
+
+				if(typeof obj === 'string'){
+					masterResult.push('"' + obj + '"');
+
+				}
+
+				if(typeof obj === 'boolean'){
+					masterResult.push(obj.toString());
+				}
+
+
+				if(Array.isArray(obj)){
+					console.log("thinks i'm an array")
+					stringifyArray(obj);
+				}
+
+				if(typeof obj === 'object'){
+					console.log("thinks i'm a default object");
+					stringifyObject(obj);
+				}
+
+		
+		//If don't need masterResult as an array, just get rid of it and return values.
+
+		//if(masterResult.length === 1){
+
+			return masterResult[0];
+		//}
+
+		//else{
+		//	return masterResult;
+		//}
+
+		};
+
+
+
+
+		// if(Array.isArray(obj)){
+				
+		// 		//subArray.push(furtherArray);
+		// 		for(var i=0; i< obj.length; i++){
+					
+		// 			stringifyParts(obj[i], arrayLevel[subArrayCounter]);
+		// 			subArrayCounter++;
+					
+		// 		}
+		// }
 
 	
 
 
-		 if(typeof obj === 'number'){
-		    arrayLevel.push(obj);
-		   }
+		//  if(typeof obj === 'number'){
+		//     arrayLevel.push(obj);
+		//    }
 
-		  if(typeof obj === 'string'){
-		    arrayLevel.push(obj);
-		   }
+		//   if(typeof obj === 'string'){
+		//     arrayLevel.push(obj);
+		//    }
 
-		  if(typeof obj === 'boolean'){
-		    	arrayLevel.push(obj);
-		    }
+		//   if(typeof obj === 'boolean'){
+		//     	arrayLevel.push(obj);
+		//     }
 
 
-		  if(obj && obj.hasOwnProperty('{}')){		
-				var newObj = {};
-					for(var key in obj){
+		//   if(obj && obj.hasOwnProperty('{}')){		
+		// 		var newObj = {};
+		// 			for(var key in obj){
 
-						if(typeof key === 'number' || typeof key === 'string' && checkStringNumberBoolean(obj[key])){
-							newObj[key] = obj[key];
+		// 				if(typeof key === 'number' || typeof key === 'string' && checkStringNumberBoolean(obj[key])){
+		// 					newObj[key] = obj[key];
 
-							}
-						}
+		// 					}
+		// 				}
 
-					arrayLevel.push(newObj);
-				}
+		// 			arrayLevel.push(newObj);
+		// 		}
 
-		  else{
+		//   else{
 
-		  	// if(arrayLevel != stringifiedJSONArray){
-		  	// 	arrayLevel.push('null');
-		  	// }
-		    	//return {};
-		    	// or null if this other element was within an array?  Can use if arrayLevel === stringifiedJSONArray to determine whether in array or not.
-		    }
+		//   	// if(arrayLevel != stringifiedJSONArray){
+		//   	// 	arrayLevel.push('null');
+		//   	// }
+		//     	//return {};
+		//     	// or null if this other element was within an array?  Can use if arrayLevel === stringifiedJSONArray to determine whether in array or not.
+		//     }
 
-	};
+	// };
 
 
 
@@ -90,12 +163,12 @@ var stringifyJSON = function(obj) {
 
   	// 	}
 
-  	// };
+//   	// };
 
-  	stringifyParts(obj, stringifiedJSONArray);
+//   	stringifyParts(obj);
 
 
-  	var stringifiedJSON = stringifiedJSONArray.join("");
+//   	var stringifiedJSON = stringifiedJSONArray.join("");
 
-  	return stringifiedJSON;
-};
+//   	return stringifiedJSON;
+// };
