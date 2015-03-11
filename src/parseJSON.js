dcd9c6011444325126e740fc74a322ee56ca55c9
Lauren <lhspiegel@gmail.com>
2015-03-10 17:56:27 -0700

    Begins to implement parseJSON.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,84 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+ 
+
+
+  var parseArray = function(jsonArray){
+    jsonArray = jsonArray.slice(1, -1);
+    jsonArray = jsonArray.split(",");
+   
+    var newArray = [];
+
+    for(var i=0; i<jsonArray.length; i++){
+      newArray.push(parseJSON(jsonArray[i]));
+    }
+
+    return newArray;
+
+  };
+
+
+  var parseObject = function(object){
+
+
+   object = object.slice(1, -1);
+   var keyValuePairArray = object.split(",");
+
+   var newObj = {};
+
+   for(var j=0; j< keyValuePairArray.length; j++){
+     
+    var singlePairArray = keyValuePairArray[j].split(":");
+
+    newObj[parseJSON(singlePairArray[0])] = parseJSON(singlePairArray[1]);
+
+
+   }
+
+  return newObj;
+
+
+  };
+  
+  if(json === 'true'){
+    return true;
+  }
+
+  if(json === 'false'){
+    return false;
+  }
+
+  if(json === 'null'){
+    return null;
+  }
+
+  if(!Number.isNaN(Number(json))){
+    
+    return Number(json);
+  }
+
+
+  if(typeof json === 'string' && json.charAt(0) === '{'){
+
+    return parseObject(json);
+  }
+
+  if(typeof json === 'string' && json.charAt(0) === '['){
+
+    return parseArray(json);
+
+  }
+
+ 
+
+
+ //For string case:
+
+  else{
+
+    return json.slice(1, -1);
+  }
+
+
 };
